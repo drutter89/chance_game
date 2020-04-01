6b5235d78170788476bf2720c529532bb1d82447
drutter <drutter@homeaway.com>
2020-03-31 22:16:51 -0500

    trying a line break instead

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ log(chalk.green(
 	'I am a green line ' +
 	chalk.blue.underline.bold('with a blue substring') +
     ' that becomes green again!',
-    <br></br>
+    <br/>
 ));
 
 log(chalk.yellow(
