d2d87582cad825f9c8c47d6d4096e2aa2a33182e
drutter <drutter@homeaway.com>
2020-03-31 22:40:25 -0500

    got the intro message going play with Chalk formatting. Remembered to not commit every change I make and from this point will have much more limited commits.

--- index.js
+++ index.js
@@ -17,11 +17,28 @@ log(chalk.red('Hello', chalk.underline.bgBlue('world') + '!'));
 log(chalk.green(
 	'I am a green line ' +
 	chalk.blue.underline.bold('with a blue substring') +
-    ' that becomes green again! <br/> '
+    ' that becomes green again!'
 ));
 
 
-
+log(chalk.black(
+    '========================================================================================================'
+));
 log(chalk.yellow(
     "Lets lay down the ground rules for the game!"
+));
+
+log(chalk.red(
+    'So here is how it works. The game will start with two options to pick from. You need to guess which option is correct'
+))
+
+log(chalk.green(
+	"It's going to be up to you to "  +
+	chalk.blue.bgGreen.underline.bold('to choose the ONE CORRECT OPTION'), 
+    ' if you choose correctly your choices double and you choose again!'
+));
+
+log(chalk.blackBright(
+    "There is only " + chalk.white.bgBlack.underline.bold("ONE CORRECT OPTION") + " each time",
+    "Will you take your time, or follow your gut.......think you've got the Luck of The Gods?"
 ));
